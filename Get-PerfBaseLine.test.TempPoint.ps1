234de276841ec9e3dd2a8f2732c3f93b2e839f3d
Darrin Britton <brittod@wwu.edu>
2018-08-30 17:40:27 -0700

    Fixed formatting

--- Get-PerfBaseLine.test.TempPoint.ps1
+++ Get-PerfBaseLine.test.TempPoint.ps1
@@ -101,17 +101,17 @@ Begin {
                 [Management.Automation.ErrorRecord]$e = $_
 
                 # retrieve information about runtime error
-				$info = [PSCustomObject]@{
-					
-					Exception = $e.Exception.Message
-					Reason    = $e.CategoryInfo.Reason
-					Target    = $e.CategoryInfo.TargetName
-					Script    = $e.InvocationInfo.ScriptName
-					Line	  = $e.InvocationInfo.ScriptLineNumber
-					Column    = $e.InvocationInfo.OffsetInLine
-					
-				}
-				
+                $info = [PSCustomObject]@{
+
+                Exception = $e.Exception.Message
+                Reason    = $e.CategoryInfo.Reason
+                Target    = $e.CategoryInfo.TargetName
+                Script    = $e.InvocationInfo.ScriptName
+                Line      = $e.InvocationInfo.ScriptLineNumber
+                Column    = $e.InvocationInfo.OffsetInLine
+
+                }
+
                 # output information. Post-process collected info, and log info (optional)
                 Write-output -InputObject $info
 
